a98545739e86f4641294ead300ca50904b377234
Piotr Wilczynski <delwing@gmail.com>
2022-04-07 21:53:51 +0200

    releaseing package preparation

---
+++ .github/workflows/prepare_package.py
@@ -0,0 +1,34 @@
+from posixpath import dirname
+from zipfile import ZipFile
+import os
+import sys
+from datetime import datetime
+
+def addDirToZip(zipObj, dirName):
+    for folderName, subfolders, filenames in os.walk(dirName):
+        for filename in filenames:
+            filePath = os.path.join(folderName, filename)
+            basePath = os.path.relpath(filePath, dirName)
+            zipObj.write(filePath, "mudlet-release/" + dirName + "/" + basePath)
+
+with ZipFile("mudlet-release.zip", "w") as zipObj:
+    addDirToZip(zipObj, "vendor")
+    zipObj.write("mudlet-release.php", "mudlet-release/mudlet-release.php")
+
+
+version = sys.argv[1]
+now = datetime.now()
+
+with open('info.template.json', "r+") as text_file:
+        texts = text_file.read()
+        texts = texts.replace("@version@", version)
+        texts = texts.replace("@date@", now.strftime("%Y-%m-%d %H:%M:%S"))
+with open('info.json', "w") as text_file:
+    text_file.write(texts)
+
+with open('mudlet-release.php', "r+") as text_file:
+    texts = text_file.read()
+    texts = texts.replace("@version@", version)
+with open('mudlet-release.php', "w") as text_file:
+    text_file.write(texts)
+
--- mudlet-release.php
+++ mudlet-release.php
@@ -1,48 +1,51 @@
 <?php
 
 /**
- * Plugin Name:       Mudlet releasees
+ * Plugin Name:       Mudlet release
  * Description:       Adds support for automatic Mudlet release posts creation
- * Version:           1.0.0
+ * Version:           @version@
  * Author:            Piotr Wilczynski
  */
+
+defined('ABSPATH') || exit;
 
 require("vendor/autoload.php");
 
 const GITHUB_API_URL = "https://api.github.com/repos/Mudlet/Mudlet/";
 
-class MudletRelease {
+class MudletRelease
+{
 
     const SHORTCODE = "MudletRelease";
     const RELEASE_CATEGORY = 173;
 
-    private $api;
     private $parsedown;
+    private $version;
 
-    function __construct() {
-        $this->api = new RestClient([
-            'base_url' => GITHUB_API_URL
-        ]);
+    function __construct()
+    {
+        $this->version = '@version@';
         $this->parsedown = new Parsedown();
 
-        add_shortcode( self::SHORTCODE, array($this, 'mudlet_release'));
-        add_action( 'wp_ajax_post_newest_release', array($this, 'mudlet_post_release'));  
-        add_action( 'wp_ajax_nopriv_post_newest_release', array($this, 'mudlet_post_release')); 
-        add_action( 'save_post', array($this, 'invalidate_transient'), 10, 2);
+        add_shortcode(self::SHORTCODE, array($this, 'mudlet_release'));
+        add_action('wp_ajax_post_newest_release', array($this, 'mudlet_post_release'));
+        add_action('wp_ajax_nopriv_post_newest_release', array($this, 'mudlet_post_release'));
+        add_action('save_post', array($this, 'invalidate_transient'), 10, 2);
+        new MudletUpdateCheck($this->version);
     }
 
-    function mudlet_release( $atts, $content ) {
+    function mudlet_release($atts, $content)
+    {
         $transient_name = $this->get_transient_name($content);
         $body = get_transient($transient_name);
-        if (true || !$body) {
-            $response = $this->api->get("releases/tags/Mudlet-$content");
-            $result = $response->decode_response();
-            if($response->info->http_code == 200) {
-               $body = $this->parsedown->text($result->body);
-               set_transient($transient_name, $body, MONTH_IN_SECONDS);
+        if (!$body) {
+            $result = GetHttpWrapper::get(GITHUB_API_URL . "releases/tags/Mudlet-$content");
+            if ($result) {
+                $body = $this->parsedown->text($result->body);
+                set_transient($transient_name, $body, MONTH_IN_SECONDS);
             } else {
                 $body = "Can't get releases post for $content";
-                if(is_user_logged_in()) {
+                if (is_user_logged_in()) {
                     $body .= '<br>Message: ' . $result->message;
                 }
             }
@@ -50,10 +53,11 @@ class MudletRelease {
         return $body;
     }
 
-    function invalidate_transient( $post_ID, WP_Post $post ) {
+    function invalidate_transient($post_ID, WP_Post $post)
+    {
         if (has_shortcode($post->post_content, self::SHORTCODE)) {
             $regex = '/\[' . self::SHORTCODE . '](.*?)\[\/' . self::SHORTCODE . ']/';
-            if( preg_match( $regex, $post->post_content, $matches ) ) {
+            if (preg_match($regex, $post->post_content, $matches)) {
                 $version = $matches[1];
                 delete_transient($this->get_transient_name($version));
                 $this->mudlet_release(null, $version);
@@ -61,35 +65,37 @@ class MudletRelease {
         }
     }
 
-    private function get_transient_name($version) {
+    private function get_transient_name($version)
+    {
         return "mudlet-release-$version";
     }
 
-    private function has_release_post_already($version) {
+    private function has_release_post_already($version)
+    {
         $args = array(
             'meta_key' => 'release-post',
             'value' => $version,
-         );
-         $query = new WP_Query($args);
-         return $query->have_posts();
+        );
+        $query = new WP_Query($args);
+        return $query->have_posts();
     }
 
-    function mudlet_post_release() {
+    function mudlet_post_release()
+    {
         $result = $this->api->get('releases/latest')->decode_response();
         if ($result->tag_name) {
             $tag_name = preg_replace('/Mudlet\-/', '', $result->tag_name);
             if ($this->has_release_post_already($tag_name)) {
                 die();
             }
-            $default_language = pll_default_language();
             $languages = pll_languages_list();
             $translations = array();
-            foreach($languages as $code) {
+            foreach ($languages as $code) {
                 $release_category = pll_get_term(self::RELEASE_CATEGORY, $code);
                 $post_id = wp_insert_post(array(
                     'post_title' => $result->name,
-                    'post_content' => '['. self::SHORTCODE .']' . $tag_name . '[/'. self::SHORTCODE .']',
-                    'post_status' => 'publish',
+                    'post_content' => '[' . self::SHORTCODE . ']' . $tag_name . '[/' . self::SHORTCODE . ']',
+                    'post_status' => 'draft',
                     'post_category' => array($release_category)
                 ));
                 add_post_meta($post_id, 'release-post', $tag_name, true);
@@ -101,7 +107,134 @@ class MudletRelease {
             die();
         }
     }
+}
 
+class MudletUpdateCheck
+{
+
+    private $version;
+    private $cache_key;
+
+    function __construct($version)
+    {
+        $this->version = $version;
+        $this->slug = plugin_basename(__DIR__);
+        $this->cache_key = 'mudlet_release';
+        add_filter('site_transient_update_plugins', array($this, 'push_update'));
+        add_filter('plugins_api', array($this, 'info'), 20, 3);
+    }
+
+    public function request_info()
+    {
+        return GetHttpWrapper::get('https://github.com/Delwing/mudlet-release-plugin/releases/latest/download/info.json', $this->cache_key);
+    }
+
+    function info($res, $action, $args)
+    {
+
+        if ('plugin_information' !== $action) {
+            return $res;
+        }
+
+
+        if ($this->slug !== $args->slug) {
+            return $res;
+        }
+
+        $remote = $this->request_info();
+        if (!$remote) {
+            return $res;
+        }
+
+        $res = new stdClass();
+        $res->name = $remote->name;
+        $res->plugin = plugin_basename(__FILE__);
+        $res->slug = $this->slug;
+        $res->author = $remote->author;
+        $res->author_profile = $remote->author_profile;
+        $res->version = $remote->version;
+        //$res->tested = $remote->tested;
+        $res->requires = $remote->requires;
+        $res->requires_php = $remote->requires_php;
+        $res->download_link = $remote->download_url;
+        $res->trunk = $remote->download_url;
+        $res->last_updated = $remote->last_updated;
+        $res->sections = array(
+            'description' => $remote->sections->description
+        );
+
+        return $res;
+    }
+
+
+    function push_update($transient)
+    {
+        if (empty($transient->checked)) {
+            return $transient;
+        }
+
+        $remote = $this->request_info();
+
+        $res = new stdClass();
+        $res->slug = $remote->slug;
+        $res->plugin = plugin_basename(__FILE__);
+        $res->package = $remote->download_url;
+
+        if ($remote && version_compare($this->version, $remote->version, '<')) {
+            $res->new_version = $remote->version;
+            $transient->response[$res->plugin] = $res;
+            $transient->checked[$res->plugin] = $remote->version;
+        } else {
+            $res->new_version = $this->version;
+            $transient->no_update[$res->plugin] = $res;
+        }
+
+        return $transient;
+    }
+}
+
+class GetHttpWrapper
+{
+
+    private $response;
+
+    function __construct($url, $transient = false)
+    {
+        $this->response = $transient ? get_transient($transient) : false;
+        if (!$this->response) {
+            $remote = wp_remote_get(
+                $url,
+                array(
+                    'timeout' => 10,
+                    'headers' => array(
+                        'Accept' => 'application/json'
+                    )
+                )
+            );
+            if ($this->is_ok($remote)) {
+                $this->response = json_decode(wp_remote_retrieve_body($remote));
+                if ($transient && $this->response) {
+                    set_transient($transient, $this->response, DAY_IN_SECONDS);
+                }
+            }
+        }
+    }
+
+    static function get($url, $transient = false)
+    {
+        $wrapper = new GetHttpWrapper($url, $transient);
+        return $wrapper->get_body();
+    }
+
+    private function is_ok($remote)
+    {
+        return !is_wp_error($remote) && 200 == wp_remote_retrieve_response_code($remote) && !empty(wp_remote_retrieve_body($remote));
+    }
+
+    function get_body()
+    {
+        return $this->response;
+    }
 }
 
 new MudletRelease();
